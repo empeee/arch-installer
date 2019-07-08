89ef6d299d51312746ce99d961bd9fdc9bb31f96
Mark Pude <markpude@gmail.com>
2019-07-08 15:55:58 -0400

    updates

---
+++ extras-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm chromium ttf-liberation octave
+# qucs needs QT4 installed... from source... yee-ikes...
+# only do this if you really need qucs, look for a better alternative
+#yay -S --noconfirm qucs qucs-s
