0634806086b68710859bc11e4137edd4a4dcca4c
Mark Pude <markpude@gmail.com>
2019-07-07 20:48:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ TIMEZONE = 'America/New_York'
 KEYMAP = 'us'
 
 timedatectl set-ntp true
+
+parted -s $DRIVE \
+    mklabel msdos \
+    mkpart primary fat32 0 300M \
+    set 1 esp on
+    mkpart primary ext4 300M 20.3G \
+    mkpart primary ext4 20.3G 100%
+
+mkfs.fat -F32 "${DRIVE}1"
+mkfs.ext4 "${DRIVE}2"
+mkfs.ext4 "${DRIVE}3"
